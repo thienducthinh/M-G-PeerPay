4827038c83451b2e362eb12ad0b841c134beef88
jrchilton30 <jrchilton30@gmail.com>
2023-09-26 15:35:58 -0700

    Updated code

--- balance.js
+++ balance.js
@@ -1,87 +1,39 @@
 $(document).ready(function () {
-    const userBalance = document.getElementById('balance');
-    // Parse the query parameter from the URL
-    var urlParams = new URLSearchParams(window.location.search);
-    var userEmail = urlParams.get('email');
+  const userBalance = document.getElementById('balance');
+  // Parse the query parameter from the URL
+  var urlParams = new URLSearchParams(window.location.search);
+  var userEmail = urlParams.get('email');
 
-    // Now userEmail contains the email address
-    console.log('User Email:', userEmail);
-
-
-        // Load and parse the CSV file using jQuery
-        // $.get('Users.csv', function (data) {
-        //     console.log(data)
-        //     // Split CSV data into rows
-        //     const rows = data.split('\n');
-
-        //     console.log(rows)
-
-        //     // Loop through each row to check for the user
-        //     let userFound = false;
-        //     for (let i = 1; i < rows.length; i++) { // Start from 1 to skip the header row
-        //         const [csvEmail, csvPassword, csvBalance] = rows[i].split(',');
-
-        //         if (csvEmail === userEmail) {
-        //             userFound = true;
-        //                 // Redirect to the next page if email and password match
-        //                 userBalance.textContent = csvBalance;
-        //                 console.log()
-
-        //     }
-        // }
-
-        // });
-        $.get("?tableName=users", function(table){
-            //   console.log(appUsers);
-            let userFound = false;
-            appUsers = JSON.parse(table);
-            appUsers.forEach(function(user){
-                if (user.email === userEmail) {
-                    userFound = true;
-                    userBalance.textContent = user.M_and_G;
-                }
-            });
-            
-            if (!userFound) {
-                errorText.textContent = 'User not found';
-            }
-        });
+  // Now userEmail contains the email address
+  console.log('User Email:', userEmail);
+  $.get("?tableName=users", function(table){
+      //   console.log(appUsers);
+      let userFound = false;
+      appUsers = JSON.parse(table);
+      appUsers.forEach(function(user){  
+          userFound = true;
+          userBalance.textContent = user.M_and_G;
+      });
+      
+  });
 
 });
 
-function getMandGBalance(emailToFind) {
-    // Iterate through the appUsers array
-    for (let i = 0; i < appUsers.length; i++) {
-      const user = appUsers[i];
-  
-      // Check if the current user's email matches the emailToFind
-      if (user.email === emailToFind) {
-        // Return the M_and_G balance for the matching user
-        return user.M_and_G;
-      }
-    }
-  
-    // If the email address is not found, you can return an error message or handle it as needed
-    return "Email not found"; // Example error message
-  }
-  
-
 var appUsers;
-  
+
 function getUsers(){
 
 $.get("?tableName=users", function(table){
 //   console.log(appUsers);
-  $("#usersTable").empty();
-  appUsers = JSON.parse(table);
-  appUsers.forEach(function(user){
+$("#usersTable").empty();
+appUsers = JSON.parse(table);
+appUsers.forEach(function(user){
 
-    $("#usersTable").append("<tr><td width=200px>" + user.email + "</td> <td width=50px>" + 
-    	user.password + "</td><td width=170px>" + user.M_and_G + "</td></tr>");
-  });
+  $("#usersTable").append("<tr><td width=200px>" + user.email + "</td> <td width=50px>" + 
+    user.password + "</td><td width=170px>" + user.M_and_G + "</td></tr>");
+});
 //   $("#users").append("<div id=''> </div>")
 
 }); //end $.get
 
 }
-
