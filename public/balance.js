7e725d70077c25ec999fb1e58dcd5794f69a6c9d
JasmineChouu <mchou9@asu.edu>
2023-10-02 23:57:42 -0700

    updated dashboard, balance

--- public/balance.js
+++ public/balance.js
@@ -24,32 +24,6 @@
 //   });
 // }
 
-// $(document).ready(function () {
-//   const userBalance = document.getElementById('balance');
-//   const userName = document.getElementById('dashboard-name');
-//   // Parse the query parameter from the URL
-//   var urlParams = new URLSearchParams(window.location.search);
-//   var userEmail = urlParams.get('email');
-
-//   // Now userEmail contains the email address
-//   console.log('User Email:', userEmail);
-//   $.get('/api/users?email=' + encodeURIComponent(userEmail), function(user){
-//     console.log(user)
-//       userBalance.textContent = user[0].M_and_G;
-//       userName.textContent = user[0].name;
-//   });
-
-// }); 
-
-// function getUsers(){
-//   $.get('/api/users', function(users){
-//     $("#usersTable").empty();
-//     users.forEach(function(user){
-//       $("#usersTable").append("<tr><td width=200px>" + user.email + "</td><td width=170px>" + user.M_and_G + "</td></tr>");
-//     });
-//   });
-// }
-
 $(document).ready(function () {
   const userBalance = document.getElementById('balance');
   const userName = document.getElementById('dashboard-name');
