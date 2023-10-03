150981e2ca905d1484da79c1485a940b216a992f
JasmineChouu <mchou9@asu.edu>
2023-10-03 03:00:17 -0700

    back to origin

--- public/balance.js
+++ public/balance.js
@@ -1,29 +1,3 @@
-// $(document).ready(function () {
-//   const userBalance = document.getElementById('balance');
-//   const userName = document.getElementById('dashboard-name');
-//   // Parse the query parameter from the URL
-//   var urlParams = new URLSearchParams(window.location.search);
-//   var userEmail = urlParams.get('email');
-
-//   // Now userEmail contains the email address
-//   console.log('User Email:', userEmail);
-//   $.get('/api/users?email=' + encodeURIComponent(userEmail), function(user){
-//     console.log(user)
-//       userBalance.textContent = user[0].M_and_G;
-//       userName.textContent = user[0].name;
-//   });
-
-// }); 
-
-// function getUsers(){
-//   $.get('/api/users', function(users){
-//     $("#usersTable").empty();
-//     users.forEach(function(user){
-//       $("#usersTable").append("<tr><td width=200px>" + user.email + "</td><td width=170px>" + user.M_and_G + "</td></tr>");
-//     });
-//   });
-// }
-
 $(document).ready(function () {
   const userBalance = document.getElementById('balance');
   const userName = document.getElementById('dashboard-name');
@@ -33,42 +7,21 @@ $(document).ready(function () {
 
   // Now userEmail contains the email address
   console.log('User Email:', userEmail);
-  $.get("?tableName=users", function(table){
-      //   console.log(appUsers);
-      let userFound = false;
-      appUsers = JSON.parse(table);
-      appUsers.forEach(function(user){  
-        if (userEmail === user.email) {
-          userFound = true;
-          userBalance.textContent = user.M_and_G;
-          userName.textContent = user.name;
-        }
-          
-      });
-      
+  $.get('/api/users?email=' + encodeURIComponent(userEmail), function(user){
+    console.log(user)
+      userBalance.textContent = user[0].M_and_G;
+      userName.textContent = user[0].name;
   });
 
-});
-
-var appUsers;
+}); 
 
 function getUsers(){
-
-$.get("?tableName=users", function(table){
-//   console.log(appUsers);
-$("#usersTable").empty();
-const friendEmail = document.getElementById('friendEmail');
-friendEmail.options.length = 0;
-console.log(friendEmail)
-appUsers = JSON.parse(table);
-appUsers.forEach(function(user){
-  $("#usersTable").append("<tr><td width=200px>" + user.email + "</td><td width=170px>" + user.M_and_G + "</td></tr>");
-  friendEmail.add(new Option(user.email, user.email))
-});
-//   $("#users").append("<div id=''> </div>")
-
-}); //end $.get
-
+  $.get('/api/users', function(users){
+    $("#usersTable").empty();
+    users.forEach(function(user){
+      $("#usersTable").append("<tr><td width=200px>" + user.email + "</td><td width=170px>" + user.M_and_G + "</td></tr>");
+    });
+  });
 }
 
 function transfer() {
